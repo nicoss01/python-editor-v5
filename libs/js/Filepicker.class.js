d41c9357ffe16d98bc02188dec5d085a1d7aab3a
Nicolas GRILLET <n.grillet01@gmail.com>
2022-04-20 23:06:07 +0200

    test

--- libs/js/Filepicker.class.js
+++ libs/js/Filepicker.class.js
@@ -12,7 +12,7 @@ Filepicker.GoogleDrive.init = function(){
 	console.log(typeof gapi);
 	if(typeof gapi=="undefined"){
 		Utils.log.text("Google chargement...");
-		window.onload = function(){
+		window.addEventListener("load", function(event) {
 			var script = document.createElement("script");
 			script.src = "https://www.google.com/jsapi?key="+Filepicker.GoogleDrive.options.developerKey;
 			script.onload=function(){
@@ -25,7 +25,7 @@ Filepicker.GoogleDrive.init = function(){
 				document.body.appendChild(script2);
 			};
 			document.body.appendChild(script);
-		};
+		});
 	}else{
 		Filepicker_GoogleDrive();
 	}
